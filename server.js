1d45c325fd0471024a3b425f6b7b519d10a617a4
jothilakshmi kodeeswaran <jothilakshmikodeeswaran@gmail.com>
2025-07-24 14:04:06 -0400

    updated

--- server.js
+++ server.js
@@ -11,8 +11,22 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const PROD_URL = process.env.PROD_URL;
 
-app.use(cors({origin: 'http://localhost:5173'}));
+// 1
+const whitelist = ["http://localhost:3000", PROD_URL];
+// 2
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+// 3
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
